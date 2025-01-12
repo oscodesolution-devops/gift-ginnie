853827d233dac433a0776eb983fef8d5a0eedf02
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 14:41:53 +0530

    add : route for privacy policy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 
 function App() {
   return (
     <Router>
       <MainLayout>
         <Routes>
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </MainLayout>
