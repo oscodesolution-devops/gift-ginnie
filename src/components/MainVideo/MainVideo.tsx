b784d9fa13e64456924640f48a212a1ffabb01dc
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 22:20:13 +0530

    add : main video component

---
+++ src/components/MainVideo/MainVideo.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+export default function MainVideo() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch((error) => {
+        console.log("Autoplay was prevented:", error);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="video-section w-screen h-screen">
+      <video
+        ref={videoRef}
+        src="https://framerusercontent.com/assets/5xFVd6oskM9qwT0cSWrJIT0oUY.mp4"
+        className="w-full h-full object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+        controls={false}
+      />
+    </div>
+  );
+}
