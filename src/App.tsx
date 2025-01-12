dd0937d2503452a54ea28b42fc1d1f4dbbdbf85a
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 13:53:23 +0530

    add : route for page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <Router>
       <MainLayout>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </MainLayout>
     </Router>
