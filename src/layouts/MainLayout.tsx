eed3ba186c5c73993559d5abd90ee5a4c7bdf6ba
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 13:25:55 +0530

    add : main layout for all components

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-full h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
