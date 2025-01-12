a342fee0aa865a49e9f54b0bf6348e6373deddd3
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 13:11:18 +0530

    setup tailwind variables for bg colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        primaryDark: "var(--dark-p-color)",
+      },
+    },
   },
   plugins: [],
-}
+};
