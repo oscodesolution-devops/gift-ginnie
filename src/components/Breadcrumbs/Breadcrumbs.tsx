acf17a96f3084c0d263443d6cd674e9f8de2a167
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-12 22:19:38 +0530

    remove : console command

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -3,7 +3,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 export default function Breadcrumbs() {
   const navigate = useNavigate();
   const location = useLocation();
-  console.log(location);
 
   const pathSegments = location.pathname.split("/").filter(Boolean); // Remove any empty segments
 
