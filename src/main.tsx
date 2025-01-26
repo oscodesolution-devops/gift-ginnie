c7b28965f8b2041867f5adffbeb492e929db774f
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-26 13:23:15 +0530

    setup : toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position="top-center" reverseOrder={false} />
     </QueryClientProvider>
   </StrictMode>
 );
