56d471f153a576a8226ff14a8f9097809a3f877f
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-26 17:03:13 +0530

    add : api integration of popular cards

--- src/components/TrendingSection/Trending.tsx
+++ src/components/TrendingSection/Trending.tsx
@@ -1,12 +1,71 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { getPopularProducts } from "../../api/api";
+import { useAuth } from "../../context/Auth";
 import HomeHeading from "../HomeHeading/HomeHeading";
 import SlidingCards from "../SlidingCards/SlidingCards";
+import { TPopularProductItem } from "../../types/Types";
+import SlidingCards2 from "../SlidingCards/SlidingCards2";
+
+type TPopularProductCategory = {
+  id: number;
+  name: string;
+  description: string;
+  category: number;
+  images: TPopularProductItem[];
+  is_liked: boolean;
+};
 
 export default function Trending() {
+  const { accessToken, isAuthenticated } = useAuth();
+  const [token, setToken] = useState<string | null>(null);
+  const {
+    data: popularProducts,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["popularProducts", token],
+    queryFn: async () => getPopularProducts(token as string),
+    enabled: !!token,
+  });
+
+  useEffect(() => {
+    if (accessToken) {
+      setToken(accessToken);
+    }
+  }, [token, accessToken]);
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="w-full mt-20">
       <HomeHeading heading={"Trending Now"} />
-      <SlidingCards />
-      <SlidingCards />
+      {isAuthenticated ? (
+        <div>
+          {popularProducts?.data?.map((product: TPopularProductCategory) => (
+            <>
+              <div
+                className="font-bold pt-6 text-xl w-full flex justify-center"
+                key={product.id}
+              >
+                {product.name}
+              </div>
+              <SlidingCards2 card={product.images} />
+            </>
+          ))}
+        </div>
+      ) : (
+        <>
+          <SlidingCards />
+          <SlidingCards />
+        </>
+      )}
     </div>
   );
 }
