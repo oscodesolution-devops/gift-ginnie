b755f8689b932e6064e82e83ea26a2e3c7a8d72c
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-01-29 16:32:17 +0530

    create : component to track rating and review

---
+++ src/components/Review/Review.tsx
@@ -0,0 +1,73 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { postRating } from "../../api/api";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../../context/Auth";
+import toast from "react-hot-toast";
+
+export default function ReviewComponent() {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [review, setReview] = useState("");
+  const { productId } = useParams();
+  const { accessToken } = useAuth();
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return postRating(accessToken as string, parseInt(productId as string));
+    },
+    onSuccess: (data) => {
+      toast.success(data.message);
+    },
+    onError: () => {
+      toast.error("Not able to add the rating");
+    },
+  });
+
+  const handleSubmit = () => {
+    if (rating === 0) {
+      alert("Please select a rating!");
+      return;
+    }
+    console.log("Review submitted:", { rating, review });
+    setRating(0);
+    setReview("");
+    mutation.mutate();
+  };
+
+  return (
+    <div className="p-6 rounded-lg mt-10 shadow-lg bg-white dark:bg-black dark:text-white dark:border-2 dark:border-white max-w-md mx-auto">
+      <h2 className="text-xl font-semibold mb-3">Rate Your Experience</h2>
+
+      <div className="flex space-x-2 mb-4">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <FaStar
+            key={star}
+            className={`cursor-pointer text-2xl ${
+              (hover || rating) >= star ? "text-yellow-400" : "text-gray-300"
+            }`}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            onClick={() => setRating(star)}
+          />
+        ))}
+      </div>
+
+      <textarea
+        value={review}
+        onChange={(e) => setReview(e.target.value)}
+        placeholder="Write your review..."
+        className="w-full dark:bg-black placeholder:dark:bg-black placeholder:dark:text-white p-2 border rounded-md resize-none focus:outline-none focus:ring-1 focus:ring-black focus:dark:ring-white"
+        rows={3}
+      />
+
+      <button
+        className="mt-3 w-full bg-black dark:bg-white text-white py-2 rounded-md uppercase dark:text-black font-semibold"
+        onClick={handleSubmit}
+      >
+        Post Review
+      </button>
+    </div>
+  );
+}
