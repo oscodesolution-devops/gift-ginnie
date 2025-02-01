55c967c2e1c700f081eb4dc4fd3d3cd767eb84da
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-02-01 16:15:14 +0530

    fix : like product functionality in different component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -7,27 +7,6 @@ import { useAuth } from "../../context/Auth";
 import { TProduct } from "../../types/Types";
 
 export default function ProductCard({ card }: { card: TProduct }) {
-  const { accessToken } = useAuth();
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn: () => {
-      return addLikeItem(accessToken as string, card.id);
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: ["allProducts"] });
-      toast.success(data.message);
-    },
-    onError: () => {
-      toast.error("Failed to add item to liked list");
-    },
-  });
-
-  const handleLikeClick = (e: React.MouseEvent) => {
-    e.stopPropagation(); // Stop event propagation
-    mutation.mutate();
-  };
-
   return (
     <div className="flex flex-col gap-2 cursor-pointer" key={card.id}>
       <div className="relative">
@@ -42,14 +21,7 @@ export default function ProductCard({ card }: { card: TProduct }) {
           </div>
         </Link>
         {/* Like button outside of Link */}
-        <div
-          onClick={handleLikeClick}
-          className={`absolute top-5 left-2 text-xl bg-[#E4E3E0] dark:bg-black/70 rounded-md px-2 py-2 hover:bg-[#E4E3E0]/70 cursor-pointer ${
-            card.is_liked ? "text-red-500" : "dark:text-white/70"
-          }`}
-        >
-          <FaHeart />
-        </div>
+        <LikeButton isLiked={card.is_liked} cardId={card.id} />
       </div>
       <Link to={`/product/${card.id}`}>
         <div className="flex justify-between items-center">
@@ -65,13 +37,71 @@ export default function ProductCard({ card }: { card: TProduct }) {
         </div>
         <div className="flex justify-between items-center text-lg">
           <div className="font-medium dark:text-white/80">
-            {card.selling_price}
+            ₹{card.selling_price}
           </div>
           <div className="font-extralight line-through dark:text-white/40">
-            {card.original_price}
+            ₹{card.original_price}
           </div>
         </div>
       </Link>
+    </div>
+  );
+}
+
+export function LikeButton({
+  isLiked,
+  cardId,
+}: {
+  isLiked: boolean;
+  cardId: number;
+}) {
+  const { accessToken } = useAuth();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      if (!accessToken) {
+        // Instead of showing toast here, throw an error
+        throw new Error("Unauthorized");
+      }
+      return addLikeItem(accessToken, cardId);
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["allProducts"] });
+      toast.success(data.message);
+    },
+    onError: (error: Error) => {
+      console.log(error, "===============");
+      if (error.message === "Unauthorized") {
+        toast.error("Please login to like products");
+      } else {
+        toast.error("Failed to add item to liked list");
+      }
+    },
+  });
+
+  const handleLikeClick = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Stop event propagation
+    mutation.mutate();
+  };
+  console.log(isLiked);
+
+  return (
+    <div
+      onClick={handleLikeClick}
+      className={`absolute top-5 left-2 text-xl bg-[#E4E3E0] dark:bg-black/70 rounded-md px-2 py-2 hover:bg-[#E4E3E0]/70 cursor-pointer ${
+        isLiked ? "text-red-500" : "dark:text-white/70"
+      }`}
+    >
+      {mutation.isPending ? (
+        <div className="flex items-center justify-center ">
+          <div className="w-full h-full px-2 py-2 border-4 border-gray-300 border-t-black dark:border-t-white rounded-full animate-spin"></div>
+        </div>
+      ) : isLiked ? (
+        <FaHeart />
+      ) : (
+        <FaHeart />
+      )}
     </div>
   );
 }
